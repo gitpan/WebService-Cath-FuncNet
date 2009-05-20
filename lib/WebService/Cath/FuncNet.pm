8ca72252d8f8fcf1ebc8162adc2985a848c9a3ba
Ian Sillitoe <isillitoe@cpan.org>
2009-05-20 11:10:53 +0000

    Import of ISILLITOE/WebService-Cath-FuncNet-0.09 from CPAN.
    gitpan-cpan-distribution: WebService-Cath-FuncNet
    gitpan-cpan-version:      0.09
    gitpan-cpan-path:         ISILLITOE/WebService-Cath-FuncNet-0.09.tar.gz
    gitpan-cpan-author:       ISILLITOE
    gitpan-cpan-maturity:     released

--- lib/WebService/Cath/FuncNet.pm
+++ lib/WebService/Cath/FuncNet.pm
@@ -6,7 +6,7 @@ WebService::Cath::FuncNet - Interface to the CATH FuncNet webservice
 
 =head1 VERSION
 
-This document describes WebService::Cath::FuncNet version 0.08
+This document describes WebService::Cath::FuncNet version 0.09
 
 =head1 SYNOPSIS
 
@@ -57,7 +57,7 @@ use File::Temp qw( tempfile );
 use Readonly;
 use Data::Dumper;
 
-our $VERSION = '0.08';
+our $VERSION = '0.09';
 
 #Readonly my $WSDL_HOST       => 'http://bsmlx47:8080';
 Readonly my $WSDL_HOST       => 'http://funcnet.eu';
